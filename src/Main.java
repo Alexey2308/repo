4a81dc06b3db66c7c362ab1c3fb3c677d31d332c
Alexey Elagin <alexyel@mail.ru>
2022-07-18 23:50:18 +0300

    Main added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] sale = {67, 87, 89};
+        SalesManager manager = new SalesManager(sale);
+        System.out.println(manager.max());
+
+    }
+}
