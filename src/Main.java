44d5fcdb8b1d826d7f57ded18ae24be4691d52f7
Alexey Elagin <alexyel@mail.ru>
2022-07-21 19:23:15 +0300

    Modified int to long

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] sale = {67, 87, 89};
+        long[] sale = {67, 87, 89};
         SalesManager manager = new SalesManager(sale);
         System.out.println(manager.max());
 
